d59bd7af8853efac449421bd3aa9d75e16390409
ivancezar <cezarivan89@gmail.com>
2026-07-14 16:31:30 +0200

    Minor modificacion. Correlation matrix added

--- src/eda_analysis.py
+++ src/eda_analysis.py
@@ -10,9 +10,9 @@ def load_and_inspect_data(file_path: str):
     # we read the data
     df = pd.read_csv(file_path, sep=";")
 
-    # we print the first 5 rows to check the strutcure. It is the same command as in R
+    # we print the first 5 rows to check the strcture. It is the same command as in R
 
-    print("\n---- First 5 rows ---")
+    print("\n --- First 5 rows ---")
     print(df.head())
 
     # Statistical summary automatized by pandas
@@ -20,6 +20,12 @@ def load_and_inspect_data(file_path: str):
     print("\n --- Statistical Summary ---")
     print(df.describe())
 
+
+    print("\n --- Correlation Matrix ---")
+    # let's select only the columns we are interested in
+    cols = ["beds", "baths", "sqft", "year_built", "final_price"]
+    print(df[cols].corr())
+    
 if __name__ == "__main__":
     CSV_FILE = "Austin_houses_test.csv"
     load_and_inspect_data(CSV_FILE)
