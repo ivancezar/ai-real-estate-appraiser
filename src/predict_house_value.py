94e821a6410e303c7fae567440600f5cc6bdf2b7
ivancezar <cezarivan89@gmail.com>
2026-07-15 15:48:06 +0200

    Minor change in house definition

--- src/predict_house_value.py
+++ src/predict_house_value.py
@@ -1,4 +1,5 @@
 import joblib
+import pandas as pd
 
 def estimate_house_value():
 
@@ -6,8 +7,12 @@ def estimate_house_value():
     model = joblib.load("austin_model.joblib")
 
     # For now the houses are defined manually, just to test
-    # Format: [beds, baths, sqft, year_built]
-    new_house = [[3, 2.5, 1800, 2015]]
+    new_house = pd.DataFrame(
+        [[3, 2.5, 1800, 2015]],
+        columns=["beds", "baths", "sqft", "year_built"]
+    )
+    
+    
 
     # We use [0] because the result of model.predict is stored in an array, and we want the first (and only) output value
     estimated_price = model.predict(new_house)[0]
